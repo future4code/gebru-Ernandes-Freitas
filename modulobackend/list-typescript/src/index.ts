0c3426c6d57b44e745e44b6056fb046600c06183
Your Name <you@example.com>
2022-05-27 19:52:01 -0400

    list-typescript

---
+++ modulobackend/list-typescript/src/index.ts
@@ -0,0 +1,6 @@
+const nome :string = "Ernandes"
+const dia: number = 22
+const mes :number = 1
+const ano :number = 1992
+
+console.log(`Olá, me chamo ${nome} , nasci no dia ${dia} do mês de ${mes} do ano de ${ano}`)
