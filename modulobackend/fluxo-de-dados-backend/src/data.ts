b46dcb3902ef4c94a7a1adf3e6cc6fba830b81d0
Your Name <you@example.com>
2022-06-07 22:01:38 -0400

    exercecicios fluxo-de-dados-backend

---
+++ modulobackend/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,22 @@
+
+ export type Produtos = {
+
+    id: string,
+    name: string,
+    price: number
+
+};
+
+export  const produtos:Produtos []=[{
+    id: "banana",
+    name:"banana",
+    price: 10
+},{
+    id: "laranja",
+    name:"laranja",
+    price: 5
+},{
+    id: "acai",
+    name:"acai",
+    price: 15
+}]
